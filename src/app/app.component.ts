5c127044f7efcf8ad8dda84c79c2ce2634e039a3
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-17 17:13:57 -0600

    Move observable to app.service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+
+import { AppService } from './app.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	template: `
+	  <b>Angular 2 Component Using Observables!</b>
+	 
+	  <h6 style="margin-bottom: 0">VALUES:</h6>
+	  <div *ngFor="let value of values">- {{ value }}</div>
+	  
+	  <h6 style="margin-bottom: 0">ERRORs:</h6>
+	  <div>Errors: {{anyErrors}}</div>
+	  
+	  <h6 style="margin-bottom: 0">FINISHED:</h6>
+	  <div>Finished: {{ finished }}</div>
+	  
+	  <button style="margin-top: 2rem;" (click)="init()">Init</button>
+	`,
+    providers: [ AppService ]
 })
 export class AppComponent {
-  title = 'app works!';
+  
+  private data: Observable<Array<number>>;
+  private values: Array<number> = [];
+  private anyErrors: boolean;
+  private finished: boolean;
+
+  constructor(
+      private appService: AppService
+  ) {}
+  
+  init() {
+      this.appService.getNumbers().subscribe(
+          value => this.values.push(Number(value)),
+          error => this.anyErrors = true,
+          () => this.finished = true
+      );
+  }
 }
